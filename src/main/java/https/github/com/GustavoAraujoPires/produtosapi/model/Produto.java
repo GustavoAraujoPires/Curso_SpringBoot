bc60c6b9e6bfa1bb0169682656362334f2282936
Gustavo Araujo Pires <gustavoaraujopires9@gmail.com>
2026-01-13 16:49:21 -0300

    Criando o mapeamento JPA para entidade Produto

--- src/main/java/https/github/com/GustavoAraujoPires/produtosapi/model/Produto.java
+++ src/main/java/https/github/com/GustavoAraujoPires/produtosapi/model/Produto.java
@@ -1,10 +1,24 @@
 package https.github.com.GustavoAraujoPires.produtosapi.model;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "produtos")
 public class Produto {
 
+    @Id
+    @Column(name = "id")
     private String id;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "descricao")
     private String descricao;
+
+    @Column(name = "preco")
     private Double preco;
 
     public String getId() {
